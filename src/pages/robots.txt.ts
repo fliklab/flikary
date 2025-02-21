1cf978086ff5b953e214d1b47d0c136e0c6b64ce
Flik Jeong <jsh852@gmail.com>
2025-02-21 16:31:09 +0900

    Disallow all agent in robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,11 +2,8 @@ import type { APIRoute } from "astro";
 import { SITE } from "@config";
 
 const robots = `
-User-agent: Googlebot
-Disallow: /nogooglebot/
-
 User-agent: *
-Allow: /
+Disallow: /
 
 Sitemap: ${new URL("sitemap-index.xml", SITE.website).href}
 `.trim();
