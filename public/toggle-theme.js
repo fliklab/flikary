d88ca64a02e8ac0654f194cbe5ae2c6e76dd86bd
Flik Jeong <performance-bot@flikary.dev>
2025-07-05 19:45:27 +0900

    feat: 테마 전환 버튼 추가, 스타일 개선

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -54,11 +54,18 @@ window.onload = () => {
     // set on load so screen readers can get the latest value on the button
     reflectPreference();
 
-    // now this script can find and listen for clicks on the control
+    // now this script can find and listen for clicks on both theme buttons
     document.querySelector("#theme-btn")?.addEventListener("click", () => {
       themeValue = themeValue === "light" ? "dark" : "light";
       setPreference();
     });
+
+    document
+      .querySelector("#theme-btn-expanded")
+      ?.addEventListener("click", () => {
+        themeValue = themeValue === "light" ? "dark" : "light";
+        setPreference();
+      });
   }
 
   setThemeFeature();
